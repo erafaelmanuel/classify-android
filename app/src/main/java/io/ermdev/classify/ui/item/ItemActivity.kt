df6eb3dcd7ef66eace4b84a82277e3573fa8777e
erafaelmanuel <erafaelmanuel@gmail.com>
2017-12-10 05:02:37 +0800

    Update the build.gradle

---
+++ app/src/main/java/io/ermdev/classify/ui/item/ItemActivity.kt
@@ -0,0 +1,15 @@
+package io.ermdev.classify.ui.item;
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+/**
+ * Created by erafaelmanuel on 11/26/2017.
+ */
+
+class ItemActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState);
+    }
+}
---
+++ app/src/main/java/io/ermdev/classify/ui/main/MainActivity.kt
@@ -0,0 +1,19 @@
+package io.ermdev.classify.ui.main;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import io.ermdev.classify.R;
+
+/**
+ * Created by erafaelmanuel on 11/26/2017.
+ */
+class MainActivity : AppCompatActivity(), MainScreen {
+
+    var presenter: MainPresenter? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
